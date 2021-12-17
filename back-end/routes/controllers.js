7d2a6c685c9a38325591fb42a073e6c53409482d
PedrazaDiego <skapet01@gmail.com>
2021-12-16 21:09:36 -0500

    post inventory and provider file added

--- back-end/routes/controllers.js
+++ back-end/routes/controllers.js
@@ -1,7 +1,6 @@
 const { Product, Provider } = require('../models/index')
 
 const getInventory = async (req, res) => {
-    console.log(req.body)
     try {
         const inventory = await Product.find()
         return res.status(200).json({ inventory })
@@ -10,7 +9,40 @@ const getInventory = async (req, res) => {
     }
 }
 
+const getProviders = async (req, res) => {
+    try {
+        const provider = await Provider.find()
+        return res.status(200).json({ provider })
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const postInventory = async (req, res) => {
+    try {
+        const item = await Product.create(req.body)
+        await item.save()
+        return res.status(201).json({ item })
+    } catch (error){
+        return res.status(500).json({ error: error.message })
+    }
+} 
+
+const postProvider = async (req, res) => {
+    try {
+        const provider = await Provider.create(req, res)
+        await provider.save()
+        return res.status(201).json({ provider })
+    } catch (error) {
+        return res.status(500).json({ error: error.message})
+    }
+}
+
+
 
 module.exports = {
     getInventory,
+    getProviders,
+    postInventory,
+    postProvider,
 }
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -5,5 +5,8 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
 router.get('/inventory', controller.getInventory)
+router.get('/providers', controller.getProviders)
+router.post('/addinventory', controller.postInventory)
+router.post('/addprovider', controller.postProvider)
 
 module.exports = router
