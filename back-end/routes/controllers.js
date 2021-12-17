e708a11dfdaffcdbe24e5c7c844c70047b4ddb3b
PedrazaDiego <skapet01@gmail.com>
2021-12-16 20:16:26 -0500

    code 200 getting inventory

--- back-end/routes/controllers.js
+++ back-end/routes/controllers.js
@@ -1,3 +1,16 @@
-const Product = require('../models/product')
-const Provider = require('../models/provider')
+const { Product, Provider } = require('../models/index')
 
+const getInventory = async (req, res) => {
+    console.log(req.body)
+    try {
+        const inventory = await Product.find()
+        return res.status(200).json({ inventory })
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+
+module.exports = {
+    getInventory,
+}
