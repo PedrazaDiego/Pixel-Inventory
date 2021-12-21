d082223d6a46e958bc1b60605fd92495e5050eeb
PedrazaDiego <skapet01@gmail.com>
2021-12-20 19:33:39 -0500

    added conditional to render the post product and provider input form

--- front-end-react/src/bars/LeftBar.js
+++ front-end-react/src/bars/LeftBar.js
@@ -7,12 +7,22 @@ import PostProvider from '../subcomponents/PostProvider'
 
 export default function LeftBar() {
 
+const [conditional, updateConditional] = useState(null)
+
+
+
 
     return (
         <div>
-            post
-            <PostProduct />
-            <PostProvider />
+            <div>
+                <h2>Add to your inventory: </h2>
+            </div>
+            <div>
+                <button onClick={() => updateConditional(0)}>Product</button>
+                <button onClick={() => updateConditional(1)}>Provider</button>
+                {conditional === 0 ? <PostProduct /> : null}
+                {conditional === 1 ? <PostProvider /> : null}
+            </div>
         </div>
     )
 }
