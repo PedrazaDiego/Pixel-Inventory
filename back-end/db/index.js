101b69df71a1fd52450d4266296b882ed2be5fc9
PedrazaDiego <skapet01@gmail.com>
2021-12-22 12:44:37 -0500

    deploying to heroku

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/inventoryDatabase'
+// let MONGODB_URI = 'mongodb://127.0.0.1:27017/inventoryDatabase'
+let MONGODB_URI='mongodb+srv://<username>:pedraza@<cluster>.i57hr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 
 mongoose
     .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
